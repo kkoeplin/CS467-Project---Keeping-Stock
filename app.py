07f4ccde159a1f1e370848378e9162a23d2203aa
thaoto22 <thaoto@oregonstate.edu>
2025-10-11 19:20:51 -0700

    Camera Working

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
