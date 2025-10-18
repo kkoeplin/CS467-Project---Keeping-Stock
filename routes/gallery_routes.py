c30aeea7959e432a0602da616b5fb8fa02e5fd16
Chong Zhen Jie <chongzhe@oregonstate.edu>
2025-10-18 09:35:54 +0800

    edited to specify data fields explicitly when reading from MongoDB

--- routes/gallery_routes.py
+++ routes/gallery_routes.py
@@ -13,7 +13,7 @@ def view():
         .find({}, {"_id": 0, "title": 1, "tags": 1, "box_id": 1, "image": 1})  # specify 1 to include or 0 to exclude fields
         .sort({"title": 1})  # specify 1 for ascending or -1 for descending
     )
-    all_boxes = list(boxes_collection.find({}).sort({"description": 1}))
+    all_boxes = list(boxes_collection.find({}, {"_id": 1, "description": 1}).sort({"description": 1}))
     all_boxes = {b["_id"]: b["description"] for b in all_boxes}
     all_tags = set()  # manually collate tags instead of reading from DB again
     for i in all_items:
@@ -39,7 +39,7 @@ def filtered_view():
     items_collection = db["test-items"]
 
     query_filter = {}
-    all_boxes = list(boxes_collection.find({}))
+    all_boxes = list(boxes_collection.find({}, {"_id": 1, "description": 1}))
     if box_description:
         box_id = next(b["_id"] for b in all_boxes if b["description"] == box_description)
         query_filter["box_id"] = box_id
