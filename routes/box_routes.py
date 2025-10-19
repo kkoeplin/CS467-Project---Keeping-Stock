bea17e7a8d94dfb58fefb9eb404709cb9eca0ccb
kkoeplin <91230923+kkoeplin@users.noreply.github.com>
2025-10-19 17:44:57 -0500

    Adding delete functionality for boxes

--- routes/box_routes.py
+++ routes/box_routes.py
@@ -6,12 +6,15 @@ import qrcode
 import io 
 import base64
 
+
 box_bp = Blueprint("box", __name__)
+
 
 # Create Box Page
 @box_bp.route("/create", methods=["GET"])
 def create_box_page():
     return render_template("create_box.html")
+
 
 # Create box
 @box_bp.route("/create", methods=["POST"])
@@ -37,6 +40,7 @@ def create_box():
         "box_id": str(inserted.inserted_id)
     })
 
+
 # List of boxes 
 @box_bp.route("/view", methods=["GET"])
 def view_boxes():
@@ -45,3 +49,25 @@ def view_boxes():
 
     boxes = list(box_db.find())  # get all boxes
     return render_template("view_box.html", boxes=boxes)
+
+
+# Delete boxes
+@box_bp.route("/delete/<box_id>", methods=["POST"])
+def delete_box(box_id):
+    print("Deleting box:", box_id)
+    db = current_app.config["DB"]
+    box_db = db["boxes"]
+
+    if not box_id:
+        return jsonify({"status": False, "error": "Need Box ID"})
+    
+    # delete the box
+    delete_result = box_db.delete_one({"_id": ObjectId(box_id)})
+
+    # Check if the box was deleted
+    if delete_result.deleted_count == 1:
+        return jsonify({"status": True, "message": "Box deleted"})
+    else:
+        return jsonify({"status": False, "error": "Box not found"})
+
+# Update Box
