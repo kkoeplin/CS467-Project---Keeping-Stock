834bf85d78a3507df26b0dfca43edbc624eba506
Chong Zhen Jie <chongzhe@oregonstate.edu>
2025-10-26 03:21:53 +0800

    refactored script for showing modal

---
+++ static/showItemModal.js
@@ -0,0 +1,38 @@
+function showItemModal(elem) {
+    const modal = document.getElementById("item-modal");
+    const item = JSON.parse(elem.dataset.item)
+    const deleteUrl = `/gallery/items/${encodeURIComponent(item._id)}`;
+
+    // mirror item card layout, adding item description and buttons
+    modal.innerHTML = `<div>
+        <img src="${ item.image.replace(/"/g, '') }">
+        <h3>${ item.title }</h3>
+        <p>${ item.description }</p>
+        <p class="item-card-box">Box: ${ item.box }</p>
+        <div>${ item.tags.map(t => `<span class="item-card-tag">${ t }</span>`).join(' ')}</div>
+        <ul>
+            <button 
+                type="button"
+                id="item-modal-delete-btn" 
+                hx-delete="${deleteUrl}"
+                hx-target="#item-card-${item._id}"
+                hx-swap="outerHTML"
+                hx-disabled-elt="this, #item-modal-close-btn"
+                hx-on::after-on-load="this.closest('dialog').close()"
+            >
+                Delete
+            </button>
+            <button 
+                type="button"
+                id="item-modal-close-btn" 
+                onclick="this.closest('dialog').close()"
+            >
+                Close
+            </button>
+        </ul>
+    </div>`;
+    modal.showModal();
+
+    // required to specify this if htmx is included in javascript
+    htmx.process(modal);
+}
